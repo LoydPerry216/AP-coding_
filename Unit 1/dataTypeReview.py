b23ca10b9df12df2d758c0152d7a1f032f9a15b3
Loyd Perry <loydperry215@gmail.com>
2025-09-03 15:12:27 +0000

    done

---
+++ Unit 1/dataTypeReview.py
@@ -0,0 +1,54 @@
+print('Loyd Perry')
+
+# W3Schools.com - our friend
+
+#1.
+#print each of the following data types:
+
+#string
+print("hello world")
+
+#boolean
+print(2 > 1)
+print(True)
+b = 3
+a = 2
+print(b > a)
+
+#integer
+print(1)
+print(1234)
+z= 32903902
+
+#float
+print(10.00)
+print(121.1212)
+
+#2.
+#create and print a list that features 2 of each data type.
+food = ["potato","salad","candy"]
+print([True, False, 10, 100, 10.5, 10.3,"I like apples"])
+
+
+
+school = "boys latin" # string
+phoneNumber = 215321344 # integer
+isEntrolled= True #Boolean
+
+# assignment
+# = single equal sign - assignment operator
+
+
+# == double equal sign - comparsion operator, specifically same as
+# != not the same
+# > greater than
+# < less than 
+# >=
+# >=
+
+print(2 == 2)
+
+# logical operator- used to compare cconditions
+print(2 > 1 and 3 == 3) # true
+print(5*3 or 20 < 10) # true
+# artithematic operator-
