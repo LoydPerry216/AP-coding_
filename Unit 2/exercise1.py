bae14f1447ff2e3767cbdce70faaec8a85ba4031
Loyd Perry <loydperry215@gmail.com>
2025-10-16 15:11:23 +0000

    done

--- Unit 2/exercise1.py
+++ Unit 2/exercise1.py
@@ -1,13 +1,40 @@
 import pandas as pd
 import nfl_data_py as nfl 
 
-schedules = nfl.import_schedules([2024])
+#schedules = nfl.import_schedules([2024])
 
-from helperFunctions import get_team_records
+from helperFunctions import get_team_records, get_season_Results_By_team
 
 #print(schedules.colums.tolist())
 
-records =get_team_records(2024)
-print(records)
+records =get_team_records(2025)
+
+#birds = get_season_Results_By_team(2025, 'PHI')
+#print(birds)
+
+top6_Teams = ['TB','IND','LA','BUF','SF','SEA']
+
+team1 =get_season_Results_By_team(2025,'TB')
+team2 =get_season_Results_By_team(2025,'IND')
+team3 =get_season_Results_By_team(2025,'LA')
+team4 =get_season_Results_By_team(2025,'SF')
+team5 = get_season_Results_By_team(2025,'SEA')
+
+
+print(team_1)
+print(team_2)
+print(team_3)
+print(team_4)
+print(team_6)
+print(team_6)
+
+# 1. Which team has the best point differential this season?
+    # The best overall Pd is INDY. They ha
+# 2. Which team has the worst point differential this season?
+     # The worst Pd is 
+# 3. Which team has the best home point differential season?
+     #
+#4. Which     
+
 
 
---
+++ Unit 2/exercise2.py
@@ -0,0 +1,38 @@
+#top6_Teams = ['TB','IND','LA','BUF','SF','SEA']
+
+#team1 =get_season_Results_By_team(2025,'TB')
+#team2 =get_season_Results_By_team(2025,'IND')
+#team3 =get_season_Results_By_team(2025,'LA')
+#team4 =get_season_Results_By_team(2025,'SF')
+#team5 = get_season_Results_By_team(2025,'SEA')
+
+
+#print(team_1)
+#print(team_2)
+#print(team_3)
+#print(team_4)
+#print(team_6)
+#8print(team_6)
+
+# 1. Which team has the best point differential this season?
+    # The best overall Pd is INDY. They ha
+# 2. Which team has the worst point differential this season?
+     # The worst Pd is SF. They have a point differiential of -3 pts.'
+# 3. Which team has the best home point differential season?
+     # The team has the best home point diffential season is 
+#4. Which has the
+
+def pdCheck():
+    print("please enter a number. pleas enter 'q' to calculate")
+    number = int(input())
+    values = []
+    calculate = 'q'
+    while calculate != 0: # while whatever IS NOT 0 DO THIS...
+        values.append(number)
+        print(values)
+        print("please enter a number")
+        number = input()
+    else:
+        print('doing calulation')    
+
+pdCheck()        
