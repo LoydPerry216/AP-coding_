c9776d9b52b2ebf0f48c9ed0910b778f0067fe5c
Loyd Perry <loydperry215@gmail.com>
2025-11-18 16:14:20 +0000

    completed assignments

---
+++ Unit 2/notes.py
@@ -0,0 +1,10 @@
+# Clarity = QUestion is easy to understand
+# Focused = The question is direct and not too broad
+# Measurable = Question can be answered/proven using data
+# Insightful = Provides new information 
+
+# Vague = Don't ask questions that aren't clear
+# Unmeasurable = Don't ask questions that can't be measured/ analyzed using statistics
+# Reocgnize Incompleteness = Don't mistake a cause with pattern without sufficient data to back it up
+# Complexity = Don't make your question too complex. Keep it simple
+
---
+++ Unit 2/researchquestionsReview.py
@@ -0,0 +1,32 @@
+from helperLogic import get_player_stats_by_name, plot_weekly_player_stats,plot_player_stat, get_team_records, get_advanced_team_records, get_position_columns, get_season_Results_By_team , weeklyPlayerStats
+import matplotlib.pyplot as plt
+
+# Columns available to research based on year and position
+columnData = get_position_columns(2024, "QB")
+# print(columnData)
+
+'1. How much does QB pass accuracy influence team wins ? '
+teamRecord = get_team_records(2024)
+print(teamRecord)
+
+qbData = weeklyPlayerStats(2024, 'QB')
+print(qbData)
+
+'J.Allen'
+'J.Hurts'
+
+playerStat= get_player_stats_by_name(2024,'J.Hurts','QB')
+print(playerStat)
+
+'Answer: Yes, there is a relationship. based on the average qb completion %, anything above 60 % is considered a good completion number'
+"also based on team records, the top 10- top teams all have had qbs that have over 65% completion percentages."
+
+
+'2. Does defensive turnovers contribute to a teams win percentage ? '
+'I believe this is question is a relation because it is trying to see if there is a relationship'
+'between turnovers and win percentages'
+"The defensive turnovers does not contribute to team wins"
+
+
+'3. Who has the most passing yards of all time ? '
+'Tom Brady has the most passing yards of all time in the NFl history.'
